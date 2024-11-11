641fc98ba07762d81e0b355f0b9dbb9664e1775a
Andre-Arcilla <aaarcilla123@gmail.com>
2024-11-11 21:06:49 +0800

    SEPARATED OLD AND NEW PROTOTYPES
    put the old files in prototype 1 and the new ones into prototype 2

---
+++ prototype V2/LSM_prototype/LSM_prototype/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,114 @@
+﻿using LSM_prototype.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSM_prototype.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObjects
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand ViewWonCommand { get; set; }
+        public RelayCommand ViewTooCommand { get; set; }
+        public RelayCommand ViewTreeCommand { get; set; }
+        public RelayCommand ViewForCommand { get; set; }
+        public RelayCommand ViewFaivCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public ViewWonModel ViewWonVM { get; set; }
+        public ViewTooModel ViewTooVM { get; set; }
+        public ViewTreeModel ViewTreeVM { get; set; }
+        public ViewForModel ViewForVM { get; set; }
+        public ViewFaivModel ViewFaivVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            ViewWonVM = new ViewWonModel();
+            ViewTooVM = new ViewTooModel();
+            ViewTreeVM = new ViewTreeModel();
+            ViewForVM = new ViewForModel();
+            ViewFaivVM = new ViewFaivModel();
+            CurrentView = HomeVM;
+
+            ViewWonCommand = new RelayCommand(o =>
+            {
+                // If already on HomeVM, reset the view to HomeVM again
+                if (CurrentView == ViewWonVM)
+                {
+                    CurrentView = HomeVM;
+                }
+                else
+                {
+                    CurrentView = ViewWonVM;
+                }
+            });
+
+            ViewTooCommand = new RelayCommand(o =>
+            {
+                if (CurrentView == ViewTooVM)
+                {
+                    // If the current view is the same, close it (set to HomeVM)
+                    CurrentView = HomeVM;
+                }
+                else
+                {
+                    CurrentView = ViewTooVM;
+                }
+            });
+
+            ViewTreeCommand = new RelayCommand(o =>
+            {
+                if (CurrentView == ViewTreeVM)
+                {
+                    // If the current view is the same, close it (set to HomeVM)
+                    CurrentView = HomeVM;
+                }
+                else
+                {
+                    CurrentView = ViewTreeVM;
+                }
+            });
+
+            ViewForCommand = new RelayCommand(o =>
+            {
+                if (CurrentView == ViewForVM)
+                {
+                    // If the current view is the same, close it (set to HomeVM)
+                    CurrentView = HomeVM;
+                }
+                else
+                {
+                    CurrentView = ViewForVM;
+                }
+            });
+
+            ViewFaivCommand = new RelayCommand(o =>
+            {
+                if (CurrentView == ViewFaivVM)
+                {
+                    // If the current view is the same, close it (set to HomeVM)
+                    CurrentView = HomeVM;
+                }
+                else
+                {
+                    CurrentView = ViewFaivVM;
+                }
+            });
+        }
+    }
+}
---
+++ prototype V2/LSM_prototype/LSM_prototype/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LSM_prototype
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void Button_Minimize(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void Button_Maximize(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow.WindowState != WindowState.Maximized)
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+            }
+        }
+
+        private void Button_Close(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            foreach (var button in new[] { Button_win1, Button_win2, Button_win3, Button_win4, Button_win5 })
+            {
+                if (button != sender && button.IsChecked == true)
+                {
+                    button.IsChecked = false;
+                }
+            }
+        }
+
+        private void Button_logout(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
